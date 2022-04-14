faee30e304f04c9105679b7a7ea171c3211d7f8a
holly <hollywilhelm670@gmail.com>
2022-04-14 12:34:39 +1000

    nav to top left open and close js added

---
+++ resources/js/scripts.js
@@ -0,0 +1,19 @@
+
+/* NAVIGATION */
+
+
+
+function openNav() {
+
+    let toggler = document.querySelector('.nav-prompt')
+
+    let navigation = document.querySelector ('.main-nav')
+
+    toggler.onclick = function(){
+    navigation.classList.toggle('open')
+
+    }
+
+
+}
+openNav()
